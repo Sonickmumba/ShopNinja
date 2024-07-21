96d0d1a4b2a50868464ff9afd00dd18c63ee1962
Sonick Mumba <kingellie.mumba@gmail.com>
2024-07-21 23:21:54 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const passport = require('passport');
 
 const router = express.Router();
 
@@ -8,5 +9,17 @@ router.get('/users/:id', userController.getUserById);
 router.post('/users', userController.createUser);
 router.put('/users/:id', userController.updateUser);
 router.delete('/users/:id', userController.deleteUser);
+
+// Authenticated user routes
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/api/users',
+  failureRedirect: '/api/login',
+  failureFlash: true
+}));
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
