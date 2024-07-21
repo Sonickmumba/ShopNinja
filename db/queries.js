502a3ecd766977743ad7a33707db4227af5dcf38
Sonick Mumba <kingellie.mumba@gmail.com>
2024-07-21 22:04:46 +0200

    Update queries.js file with register function

--- db/queries.js
+++ db/queries.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 
 const getUsers = async () => {
   const results = await pool.query('SELECT * FROM users order by id asc');
+  console.log(results.rows);
   return results.rows;
 };
 
@@ -32,29 +33,58 @@ const deleteUser = async (id) => {
   return results.rows[0];
 };
 
+// const registerUser = async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   try {
+//     // hash the password first
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     pool.query(
+//       'INSERT INTO USERS (name, email, password) VALUES ($1, $2, $3) RETURNING id', [name, email, hashedPassword], (err,results) => {
+//         if (err) throw err;
+//         res.status(201).send(`You have successfully been registerd with ID: ${results.rows[0].id}`);
+//       }
+//     )
+//   } catch (error) {
+//     res.status(500).send(error.message);
+//   }
+
+// }
+
+
+
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    // hash the password first
+    // Check if user already exists
+    const usersResult = await pool.query('SELECT * FROM users WHERE name = $1 OR email = $2', [name, email]);
+    const users = usersResult.rows;
+
+    if (users.length > 0) {
+      return res.status(409).send('User already exists');
+    }
+
+    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    pool.query(
-      'INSERT INTO USERS (name, email, password) VALUES ($1, $2, $3) RETURNING id', [name, email, hashedPassword], (err,results) => {
-        if (err) throw err;
-        res.status(201).send(`You have successfully been registerd with ID: ${results.rows[0].id}`);
-      }
-    )
+    // Insert the new user into the database
+    const result = await pool.query(
+      'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id',
+      [name, email, hashedPassword]
+    );
+
+    res.status(201).send(`User registered with ID: ${result.rows[0].id}`);
   } catch (error) {
     res.status(500).send(error.message);
   }
-
-}
+};
 
 module.exports = {
   getUsers,
   getUserById,
-  createUser,
+  // createUser,
   updateUser,
   deleteUser,
   registerUser,
