b67f45dbad73fb06ba615d88863cf9b8ac2c6061
Sonick Mumba <kingellie.mumba@gmail.com>
2024-07-21 09:45:26 +0200

    Add register user middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
+const db = require('./db/queries');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,6 +16,9 @@ app.use('/api', userRoutes);
 app.get('/', (req, res) => {
   res.json({ info: 'Node.js, Express, and Postgres API Template by Sonick Mumba' });
 });
+
+// route for user registration
+app.post('/register', db.registerUser);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
