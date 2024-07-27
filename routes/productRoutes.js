cf665bb236199f2b312b922e8c7c7191ca338b3c
Sonick Mumba <kingellie.mumba@gmail.com>
2024-07-27 14:40:46 +0200

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,18 +4,18 @@ const productController = require('../controllers/productController');
 const router = express.Router();
 
 // Route to create a new product
-router.post('/products', productController.createProduct);
+router.post('/', productController.createProduct);
 
 // Route to get products with optional filters
-router.get('/products', productController.getProducts);
+router.get('/', productController.getProducts);
 
 // Route to get a single product by ID
-router.get('/products/:id', productController.getProductById);
+router.get('/:id', productController.getProductById);
 
 // Route to update a product
-router.put('/products/:id', productController.updateProduct);
+router.put('/:id', productController.updateProduct);
 
 // Route to delete a product
-router.delete('/products/:id', productController.deleteProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
