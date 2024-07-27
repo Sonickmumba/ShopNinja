53fddaf4d3a7f7ec738f913f2610b7074fab4666
Sonick Mumba <kingellie.mumba@gmail.com>
2024-07-27 14:40:41 +0200

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const passport = require('passport');
 
 const orderController = require('../controllers/orderController');
 
 const router = express.Router();
 
-router.get('/orders', orderController.getOrders);
-router.get('/orders/:orderId', orderController.getOrderDetails);
+router.get('/', passport.authenticate('local', { session: true }), orderController.getOrders);
+router.get('/:orderId', passport.authenticate('local', { session: true }), orderController.getOrderDetails);
 
 
 module.exports= router;
