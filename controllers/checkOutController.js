ab1c83f71ef923f664b9d14d8f5d3803c7d5a472
Sonick Mumba <kingellie.mumba@gmail.com>
2024-07-27 06:10:05 +0200

    Update checkOutController.js

--- controllers/checkOutController.js
+++ controllers/checkOutController.js
@@ -3,12 +3,12 @@ const orderQueries = require("../db/orderQueries");
 const paymentService = require('../utils/paymentService');
 
 const checkout = async (req, res) => {
-  const cart_id = parseInt(req.params.cart_id, 10);
+  const cart_id = parseInt(req.params.cartId, 10);
   const { user_id, payment_details } = req.body;
 
   try {
     const cartItems = await cartsQueries.getCart(cart_id);
-
+    
     if (cartItems.length == 0) {
       return res.status(404).json({ message: "Cart is empty" });
     }
@@ -17,24 +17,29 @@ const checkout = async (req, res) => {
       (sum, item) => sum + item.price * item.quantity,
       0
     );
-    const status = 'successful';
 
-    // Step 2: Process the Payment
+    console.log('Processing payment with details:', payment_details);
     const paymentResult = await paymentService.processPayment(payment_details);
+    console.log('Payment result:', paymentResult); // Log payment result
+    
     if (!paymentResult.success) {
-      return res.status(400).json({ error: "Payment failed" });
-    } else {
-      // status = "successful";   remember to implement this. For now its the default used.
+      return res.status(400).json({ error: 'Payment failed' });
     }
+
+    // If payment is successful
+    console.log('Payment successful');
+    const status = 'successful';
 
     // create order
     const order_id = await orderQueries.createOrder(user_id, total, status);
+    // console.log(order_id)
     await orderQueries.createOrderItems(order_id, cartItems);
     await orderQueries.clearCart(cart_id);
 
     res.status(201).json({ order_id: order_id });
 
   } catch (error) {
+    console.error('Error during checkout:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
