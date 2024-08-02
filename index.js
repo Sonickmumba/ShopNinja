a694875c6cdb238a8c0f7d0d4d2726d73b56c018
Sonick Mumba <kingellie.mumba@gmail.com>
2024-08-03 00:14:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const session = require('express-session');
