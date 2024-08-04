b69f94a00b5de05df6ad7b9d74a55e84f6dfbd62
Sonick Mumba <kingellie.mumba@gmail.com>
2024-08-04 02:38:41 +0200

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -6,7 +6,6 @@ const router = express.Router();
 router.post('/', cartController.createCart);
 router.post('/:cartId', cartController.addToCart);
 router.get('/:id', cartController.getCart);
-// router.post('/cart/:cartId/checkout', cartController.checkout);
 
 
 module.exports = router;
