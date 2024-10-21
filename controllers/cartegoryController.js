13f742b03007e0c3afa38e3bef0f9ba434cd2ae5
Sonick Mumba <kingellie.mumba@gmail.com>
2024-10-21 17:02:26 +0200

    Update cartegoryController.js

--- controllers/cartegoryController.js
+++ controllers/cartegoryController.js
@@ -1,21 +1,24 @@
-const db = require('../db/cartegoryQueries');
+const db = require('../db/categoryQueries');
 
 const createCartegory = async (req, res) => {
   try {
-    const {name, description} = req.body;
+    const { name, description } = req.body;
     const cartegoryId = await db.createCartegory(name, description);
-    res.status(200).json({ message: 'Cartegory created successfully'})
+    res.status(200).json({ message: 'Cartegory created successfully', cartegoryId})
   } catch (error) {
     res.status(500).json({error: 'Internal Server Error'});
   }
 };
 
 const getCartegories = async (req, res) => {
+  console.log('trying to get cartegories11')
   try {
-    const cartegories = db.getCartegories();
+    console.log('mumba')
+    const cartegories = await db.getCartegories();
+    console.log('trying to get cartegories')
     res.status(200).json(cartegories);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error'})
+    res.status(500).json({ error: 'Internal Server Error mmm'})
   }
 };
 
