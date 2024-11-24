7ebf02b99eff63a31afa1b77ba9e7ca0b7e528fa
Sonick Mumba <kingellie.mumba@gmail.com>
2024-11-24 18:30:35 +0200

    Update jwtMiddleware.js

--- utils/jwtMiddleware.js
+++ utils/jwtMiddleware.js
@@ -1,21 +1,36 @@
 // jwtMiddleware.js
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET; // Replace with a strong secret
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
-const verifyToken = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
+const authenticateToken = (req, res, next) => {
+  const token =
+    req.headers["authorization"]?.split(" ")[1] || req.cookies?.token;
 
   if (!token) {
-    return res.status(403).json({ message: 'No token provided' });
+    return res.status(403).json({ message: "No token provided" });
   }
 
-  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+  jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: "Unauthorized" });
     }
-    req.user = { id: decoded.id }; // Save user ID to request for later use
+    req.user = user; // Save user ID to request for later use
     next();
   });
 };
 
-module.exports = verifyToken;
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+};
+
+module.exports = {
+  authenticateToken,
+  generateToken,
+};
