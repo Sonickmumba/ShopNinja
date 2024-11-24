25fc0fb54afca8cb26be0ad2456027873f327605
Sonick Mumba <kingellie.mumba@gmail.com>
2024-11-24 19:08:25 +0200

    Update jwtMiddleware.js

--- utils/jwtMiddleware.js
+++ utils/jwtMiddleware.js
@@ -3,6 +3,10 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authenticateToken = (req, res, next) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables.");
+  }
+
   const token =
     req.headers["authorization"]?.split(" ")[1] || req.cookies?.token;
 
