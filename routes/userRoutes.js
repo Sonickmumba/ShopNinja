062ff7a32c45d95e4e1b1da4a96b2045ddb8669e
Sonick Mumba <kingellie.mumba@gmail.com>
2024-11-24 22:45:32 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -225,7 +225,7 @@ router.post(
     // Optionally set the token in an HTTP-only cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "development",
+      secure: process.env.NODE_ENV === "production",
       sameSite: "Strict",
       maxAge: 3600000, // 1 hour
     });
