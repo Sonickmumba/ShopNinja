18af7083f5c47bef967553a028d7248c0d67b778
Sonick Mumba <kingellie.mumba@gmail.com>
2024-11-25 00:49:51 +0200

    Create Signup.js

---
+++ views/src/features/login/Signup.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Signup() {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '' });
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3001/api/register', formData);
+      setMessage(response.data.message);
+      console.log(response)
+
+      if (response.data.success) {
+        navigate('/home')
+      }
+    } catch (error) {
+      setMessage(error.response?.data?.message || 'Something went wrong');
+    }
+  };
+  
+  return (
+    <div className="container">
+      <h2 className="heading">Signup</h2>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="input-container">
+        <input
+            type="name"
+            name="name"
+            placeholder="Name"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="input"
+            required
+          />
+        </div>
+        <div className="input-container">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            id="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="input"
+            required
+          />
+        </div>
+
+        <div className="input-container">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            id="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="input"
+            required
+          />
+        </div>
+
+        <button type="submit" className="login-button">
+          Signup
+        </button>
+      </form>
+
+      {message && <p>{message}</p>}
+    </div>
+  )
+}
+
+export default Signup;
