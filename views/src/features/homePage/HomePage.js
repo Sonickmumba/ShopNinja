9573b4fa62073145dd0380f62d012bcbdaef358e
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-05 23:50:45 +0200

    Add header to home page

--- views/src/features/homePage/HomePage.js
+++ views/src/features/homePage/HomePage.js
@@ -3,23 +3,27 @@
 // import Carousel from "../util/Carousel";
 // import { FiMenu, FiShoppingCart, FiUser } from "react-icons/fi";
 // import checkAuthStatus from "../../utils/checkAuthStatus";
+import { MdNotifications } from "react-icons/md";
 import Footer from "./Footer";
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="homepage-container">
-      <header>ShopNinja</header>
-      <div className="search-feature-container">
+      <header>
+        <div className="header-container">
+          <p className="shop-name">ShopNinja</p>
+          <MdNotifications className="notification-icon" />
+        </div>
         <input
-          type="text"
-          className="search-bar"
-          placeholder="What are you looking for ?"
-        />
-         <Footer />
-      </div>
-      {/* <Footer /> */}
-
+        type="text"
+        className="search-bar"
+        placeholder="What are you looking for ?"
+      />
+      </header>
+      
+      <Footer />
+      
     </div>
   );
 };
