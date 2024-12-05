3158c37c61dc8927d2f685bc3861470f44a2f9e6
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-06 01:46:08 +0200

    Update HomePage.js

--- views/src/features/homePage/HomePage.js
+++ views/src/features/homePage/HomePage.js
@@ -3,7 +3,7 @@
 // import Carousel from "../util/Carousel";
 // import { FiMenu, FiShoppingCart, FiUser } from "react-icons/fi";
 // import checkAuthStatus from "../../utils/checkAuthStatus";
-import { MdNotifications } from "react-icons/md";
+import { MdNotifications, MdSearch } from "react-icons/md";
 import Footer from "./Footer";
 import "./HomePage.css";
 
@@ -16,14 +16,22 @@ const HomePage = () => {
           <MdNotifications className="notification-icon" />
         </div>
         <input
-        type="text"
-        className="search-bar"
-        placeholder="What are you looking for ?"
-      />
+          type="text"
+          className="search-bar"
+          placeholder="What are you looking for ?"
+        />
+        <button type="button" className="search-button"><MdSearch className="search-icon" /></button>
+        <div className="categories">
+          <a href="#">All</a>
+          <a href="#">Women</a>
+          <a href="#">Men</a>
+          <a href="#">Sale</a>
+          <a href="#">Electronics</a>
+          {/* <a href="#">Sports</a> */}
+        </div>
       </header>
-      
+
       <Footer />
-      
     </div>
   );
 };
