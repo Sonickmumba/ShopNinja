51a9ac1c2dcce3a753d0bc84e27ff23a40caeb78
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-05 21:47:02 +0200

    implement footer

--- views/src/features/homePage/HomePage.js
+++ views/src/features/homePage/HomePage.js
@@ -3,66 +3,23 @@
 // import Carousel from "../util/Carousel";
 // import { FiMenu, FiShoppingCart, FiUser } from "react-icons/fi";
 // import checkAuthStatus from "../../utils/checkAuthStatus";
+import Footer from "./Footer";
 import "./HomePage.css";
 
 const HomePage = () => {
-
   return (
     <div className="homepage-container">
-      <nav className="navbar">
-        <div className="logo">ShopNinja</div>
-        <ul className="navigation">
-          <li>
-            <a href="#login">Login</a>
-          </li>
-          <div className="line"></div>
-          <li>
-            <a href="#register">Register</a>
-          </li>
-          <div className="line"></div>
-          <li>
-            <a href="#orders">Orders</a>
-          </li>
-          <div className="line"></div>
-          <li>
-            <a href="#account">My Account</a>
-          </li>
-          <div className="navbar-icons">
-            {/* <FiShoppingCart /> */}
-            {/* <FiUser /> */}
-          </div>
-        </ul>
-      </nav>
+      <header>ShopNinja</header>
       <div className="search-feature-container">
         <input
           type="text"
           className="search-bar"
           placeholder="What are you looking for ?"
-          // value={searchTerm}
-          // onChange={handleSearch}
         />
+         <Footer />
       </div>
-      <footer><ul className="navigation">
-          <li>
-            <a href="#login">Login</a>
-          </li>
-          <div className="line"></div>
-          <li>
-            <a href="#register">Register</a>
-          </li>
-          <div className="line"></div>
-          <li>
-            <a href="#orders">Orders</a>
-          </li>
-          <div className="line"></div>
-          <li>
-            <a href="#account">My Account</a>
-          </li>
-          <div className="navbar-icons">
-            {/* <FiShoppingCart /> */}
-            {/* <FiUser /> */}
-          </div>
-        </ul></footer>
+      {/* <Footer /> */}
+
     </div>
   );
 };
