1f0897f40a76211dd51426a9beff2cfb3b53e714
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-14 15:17:04 +0200

    Update FooterItem.js

--- views/src/features/homePage/FooterItem.js
+++ views/src/features/homePage/FooterItem.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const FooterItem = ( { icon: Icon, size, label }) => {
+const FooterItem = ( { icon: Icon, size, label, onClick }) => {
   return (
-    <div className="footer-item">
+    <div className="footer-item" onClick={onClick} style={{ cursor: "pointer" }}>
       <Icon  size={size} />
       <span>{label}</span>
     </div>
