bfbd3ac7a14953ff4ad483c6c53e566259c28db3
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-14 14:59:42 +0200

    Update Footer.js

--- views/src/features/homePage/Footer.js
+++ views/src/features/homePage/Footer.js
@@ -2,31 +2,17 @@ import React from "react";
 import { AiOutlineHome, AiOutlineSearch, AiOutlineShoppingCart } from "react-icons/ai";
 import { BiCategory } from "react-icons/bi";
 import { FiUser } from "react-icons/fi";
-import "./Footer.css"; // Optional: for styling
+import FooterItem from "./FooterItem";
+import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer-item">
-        <AiOutlineHome size={30} />
-        <span>Home</span>
-      </div>
-      <div className="footer-item">
-        <BiCategory size={30} />
-        <span>Categories</span>
-      </div>
-      <div className="footer-item">
-        <FiUser size={24} />
-        <span>You</span>
-      </div>
-      <div className="footer-item">
-        <AiOutlineSearch size={30} />
-        <span>Search</span>
-      </div>
-      <div className="footer-item">
-        <AiOutlineShoppingCart size={30} />
-        <span>Cart</span>
-      </div>
+      <FooterItem icon={AiOutlineHome} size={30}  label="Home"/>
+      <FooterItem icon={BiCategory} size={30}  label="Categories"/>
+      <FooterItem icon={FiUser} size={30}  label="You"/>
+      <FooterItem icon={AiOutlineSearch} size={30}  label="Search"/>
+      <FooterItem icon={AiOutlineShoppingCart} size={30}  label="Cart"/>
     </div>
   );
 }
