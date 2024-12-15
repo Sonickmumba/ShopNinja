620e9a2b49a4821b8518294ee2aff295a3442ac8
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-15 08:59:52 +0200

    Add user component

---
+++ views/src/features/login/User.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { FiSettings, FiMessageSquare } from "react-icons/fi";
+import { BsCartCheck } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiMap } from "react-icons/bi";
+
+const User = ({ isSignedIn, onSignIn }) => {
+  return (
+    <div className="user-component">
+      {!isSignedIn && (
+        <div className="sign-in-section">
+          <p>Sign in for the best experience</p>
+          <button className="sign-in-button" onClick={onSignIn}>
+            Sign In
+          </button>
+        </div>
+      )}
+
+      <div className="user-options">
+        <div className="user-option">
+          <FiMessageSquare size={24} />
+          <span>Messages</span>
+        </div>
+        <div className="user-option">
+          <BsCartCheck size={24} />
+          <span>Your Orders</span>
+        </div>
+        <div className="user-option">
+          <AiOutlineHeart size={24} />
+          <span>Your Reviews</span>
+        </div>
+        <div className="user-option">
+          <BiMap size={24} />
+          <span>Address</span>
+        </div>
+        <div className="user-option">
+          <FiSettings size={24} />
+          <span>Settings</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
