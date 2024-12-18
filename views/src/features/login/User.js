0ac577fd64a73f531867304a3ad9d93bbe535c48
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-18 15:45:42 +0200

    Update User.js

--- views/src/features/login/User.js
+++ views/src/features/login/User.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { FiSettings, FiChevronRight, FiMessageSquare } from "react-icons/fi";
-import { FaSignOutAlt } from 'react-icons/fa';
+import { FaSignOutAlt } from "react-icons/fa";
 import { BsCartCheck } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
+import { MdKeyboardArrowLeft } from "react-icons/md";
 import { BiMap } from "react-icons/bi";
 import "./user.css";
 import { useSelector } from "react-redux";
@@ -14,24 +15,38 @@ const User = ({ handleSignout }) => {
 
   return (
     <div className="user-component">
+      <div className="user-profile">
+        <div className="user-back">
+          <MdKeyboardArrowLeft size={40} /> <h3>My Profile</h3>
+        </div>
+        <button type="button" className="signout-button">
+          Sign out
+        </button>
+      </div>
       {isSignedIn ? (
         <div className="profile-section">
-          <img
-            src={userProfile?.image || "default-avatar.png"}
-            alt="User Profile"
-            className="profile-picture"
-          />
-          <p className="user-name">{userProfile?.name || "Guest User"}</p>
+          <div className="pro-con">
+            <img
+              src={userProfile?.image || "default-avatar.png"}
+              alt="User Profile"
+              className="profile-picture"
+            />
+            <div className="profile-div">
+              <p className="user-name">{userProfile?.name || "Guest User"}</p>
+              <p>{userProfile?.email || "No email"}</p>
+            </div>
+          </div>
+          <FiChevronRight size={20} className="right-arrow" />
         </div>
       ) : (
         <div className="sign-in-section">
           <p>Sign in for the best experience</p>
-          <button className="sign-in-button" onClick={() => navigate('/login')}>
+          <button className="sign-in-button" onClick={() => navigate("/login")}>
             Sign In
           </button>
         </div>
       )}
-
+      <h4>Account</h4>
       <div className="user-options">
         <div className="user-option">
           <FiMessageSquare size={24} />
@@ -68,14 +83,15 @@ const User = ({ handleSignout }) => {
             <FiChevronRight size={20} className="right-arrow" />
           </div>
         </div>
-        {isSignedIn && <div className="user-option">
-          <FaSignOutAlt size={24} />
-          <div className="message-arrow" onClick={handleSignout}>
-            <span>Sign out</span>
-            <FiChevronRight size={20} className="right-arrow" />
+        {isSignedIn && (
+          <div className="user-option">
+            <FaSignOutAlt size={24} />
+            <div className="message-arrow" onClick={handleSignout}>
+              <span>Sign out</span>
+              <FiChevronRight size={20} className="right-arrow" />
+            </div>
           </div>
-        </div>}
-        
+        )}
       </div>
     </div>
   );
