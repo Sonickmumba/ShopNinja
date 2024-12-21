fde14cfae3c3aa2799b447c2ecee5f7c11f5dff4
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-21 23:38:26 +0200

    Update Cart.js

--- views/src/features/cart/Cart.js
+++ views/src/features/cart/Cart.js
@@ -38,7 +38,8 @@ const Cart = ({ cartItems, handleRemoveBtn }) => {
   return (
     <div className={styles.cartContainer}>
       <h2 className={styles.header}>My cart</h2>
-      {cartItems.length === 0 ? (<div className={styles.emptyCartMessage}>
+      {cartItems.length === 0 ? (
+        <div className={styles.emptyCartMessage}>
           <p>Your cart is empty.</p>
           <button
             className={styles.shopButton}
@@ -49,90 +50,69 @@ const Cart = ({ cartItems, handleRemoveBtn }) => {
           >
             Continue Shopping
           </button>
-        </div>) : (
-        <div className={styles.cartCardContainer}>
-        {cartItems &&
-          cartItems.map((item) => (
-            <div className={styles.cartItem} key={item.id}>
-              <img
-                className={styles.itemImage}
-                src={item.image_url}
-                alt="Classic Vest"
-              />
-              <div className={styles.itemDetails}>
-                <h3 className={styles.itemName}>{item.name}</h3>
-                <p className={styles.itemBrand}>{item.description}</p>
-                <p>quantity: {item.quantity}</p>
-                <p>Price: R{Number(item.price) * item.quantity}</p>
-              </div>
-              <button
-                className={styles.removeButton}
-                id={item.id}
-                onClick={(e) => handleRemoveBtn(e)}
-              >
-                ×
-              </button>
-            </div>
-          ))}
-      </div>
-      )}
-      {/* <div className={styles.cartCardContainer}>
-        {cartItems &&
-          cartItems.map((item) => (
-            <div className={styles.cartItem} key={item.id}>
-              <img
-                className={styles.itemImage}
-                src={item.image_url}
-                alt="Classic Vest"
-              />
-              <div className={styles.itemDetails}>
-                <h3 className={styles.itemName}>{item.name}</h3>
-                <p className={styles.itemBrand}>{item.description}</p>
-                <p>quantity: {item.quantity}</p>
-                <p>Price: R{Number(item.price) * item.quantity}</p>
-              </div>
-              <button
-                className={styles.removeButton}
-                id={item.id}
-                onClick={(e) => handleRemoveBtn(e)}
-              >
-                ×
-              </button>
-            </div>
-          ))}
-      </div> */}
-      <div className={styles.couponSection}>
-        <input
-          type="text"
-          placeholder="Apply coupon"
-          className={styles.couponInput}
-        />
-        <button className={styles.couponButton}>+</button>
-      </div>
+        </div>
+      ) : (
+        <>
+          <div className={styles.cartCardContainer}>
+            {cartItems &&
+              cartItems.map((item) => (
+                <div className={styles.cartItem} key={item.id}>
+                  <img
+                    className={styles.itemImage}
+                    src={item.image_url}
+                    alt="Classic Vest"
+                  />
+                  <div className={styles.itemDetails}>
+                    <h3 className={styles.itemName}>{item.name}</h3>
+                    <p className={styles.itemBrand}>{item.description}</p>
+                    <p>quantity: {item.quantity}</p>
+                    <p>Price: R{Number(item.price) * item.quantity}</p>
+                  </div>
+                  <button
+                    className={styles.removeButton}
+                    id={item.id}
+                    onClick={(e) => handleRemoveBtn(e)}
+                  >
+                    ×
+                  </button>
+                </div>
+              ))}
+          </div>
 
-      <div className={styles.priceDetails}>
-        <div className={styles.priceRow}>
-          <span>Sub total</span>
-          <span>R {subTotal.toFixed(2)}</span>
-        </div>
-        <div className={styles.priceRow}>
-          <span>Shipping</span>
-          <span>R {shipping.toFixed(2)}</span>
-        </div>
-        <div className={styles.priceRow}>
-          <span>Tax</span>
-          <span>R {tax.toFixed(2)}</span>
-        </div>
-        <div className={styles.priceRow}>
-          <span>Coupon applied</span>
-          <span>-R {couponDiscount.toFixed(2)}</span>
-        </div>
-        <div className={`${styles.priceRow} ${styles.total}`}>
-          <span>Total</span>
-          <span>R {total.toFixed(2)}</span>
-        </div>
-        <button className={styles.paymentButton}>Proceed to payment</button>
-      </div>
+          <div className={styles.couponSection}>
+            <input
+              type="text"
+              placeholder="Apply coupon"
+              className={styles.couponInput}
+            />
+            <button className={styles.couponButton}>+</button>
+          </div>
+
+          <div className={styles.priceDetails}>
+            <div className={styles.priceRow}>
+              <span>Sub total</span>
+              <span>R {subTotal.toFixed(2)}</span>
+            </div>
+            <div className={styles.priceRow}>
+              <span>Shipping</span>
+              <span>R {shipping.toFixed(2)}</span>
+            </div>
+            <div className={styles.priceRow}>
+              <span>Tax</span>
+              <span>R {tax.toFixed(2)}</span>
+            </div>
+            <div className={styles.priceRow}>
+              <span>Coupon applied</span>
+              <span>-R {couponDiscount.toFixed(2)}</span>
+            </div>
+            <div className={`${styles.priceRow} ${styles.total}`}>
+              <span>Total</span>
+              <span>R {total.toFixed(2)}</span>
+            </div>
+            <button className={styles.paymentButton}>Proceed to payment</button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
