ebb1bdc4ebc86ae71441a2d36e9d436f51746637
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-26 03:16:01 +0200

    Update Checkout.js

--- views/src/features/checkout/Checkout.js
+++ views/src/features/checkout/Checkout.js
@@ -1,16 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FiChevronRight } from "react-icons/fi";
+import { useSelector } from "react-redux";
 import styles from "./Checkout.module.css";
 import { useNavigate } from "react-router-dom";
 import DeliveryAddress from "./DeliveryAddress";
-// const userController = require("../../../../controllers/userController");
-// import retriveUserAddress from "../../../../controllers/userController";
 
 const Checkout = () => {
-  // implement fetch of user address from the api.
+  const [address, setAddress] = useState('');
+  const { isSignedIn, userProfile } = useSelector((state) => state.user);
 
-  const userAddress = "Jubilee Garden, Bandan Gali, Gujarat";
   const navigate = useNavigate();
+  const id = isSignedIn ? userProfile.id : null;
+
+  const fetchUserAddress = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/api/user/address/${id}`);
+
+      if (!response.ok){
+        throw new Error("User address not found");
+      }
+
+      const data = await response.json();
+
+      const userAddress = `${data.address_line1}, ${data.city}, ${data.state}, ${data.postal_code}, ${data.country}`;
+      console.log(userAddress)
+      setAddress(userAddress)
+      console.log(response)
+    } catch (error) {
+      console.error("Error fetching user address:", error);
+    }
+  }
+
+  useEffect(() => {
+    if (isSignedIn) {
+      fetchUserAddress();
+    }
+  }, [isSignedIn]);
 
   return (
     <div className={styles.paymentContainer}>
@@ -24,7 +49,7 @@ const Checkout = () => {
       </header>
 
       {/* Delivery Address */}
-      <DeliveryAddress address={userAddress} />
+      <DeliveryAddress address={address} />
 
       {/* Payment Methods */}
       <section className={styles.paymentMethods}>
