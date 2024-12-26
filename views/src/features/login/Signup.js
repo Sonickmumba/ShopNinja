41890e580b5c6c30b20ada1388ff7ee5a0799df6
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-26 03:16:08 +0200

    Update Signup.js

--- views/src/features/login/Signup.js
+++ views/src/features/login/Signup.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { signIn } from "./userSlice";
 
 function Signup() {
   const [formData, setFormData] = useState({
@@ -10,6 +12,7 @@ function Signup() {
   });
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -36,10 +39,11 @@ function Signup() {
         formData
       );
       setMessage(response.data.message);
-      console.log(response);
+      console.log(response.data.user);
 
       if (response.data.success) {
         navigate("/home");
+        dispatch(signIn(response.data.user))
       }
     } catch (error) {
       console.log(error.response);
