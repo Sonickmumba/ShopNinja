01c3a77c81029d443cc06e8128c6954b9eb6ae01
Sonick Mumba <kingellie.mumba@gmail.com>
2024-12-26 03:15:58 +0200

    Update App.js

--- views/src/App.js
+++ views/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { signIn, signOut } from "./features/login/userSlice";
 import signout from "./features/util/signout";
