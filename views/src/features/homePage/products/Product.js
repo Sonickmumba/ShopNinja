1278f6a04fe9105524e1516c446d61da59b4d355
Sonick Mumba <kingellie.mumba@gmail.com>
2025-01-01 06:49:58 +0200

    Update Product.js

--- views/src/features/homePage/products/Product.js
+++ views/src/features/homePage/products/Product.js
@@ -1,23 +1,37 @@
 // import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Product = ( {displayedProducts}) => {
+const Product = ({ displayedProducts, addToCart }) => {
 
+  const handleAddToCartBtn = (product) => {
+    console.log("Adding product to cart:", product);
+    if (!product || !product.id) {
+      console.error("Invalid product:", product);
+      return;
+    }
+    if (addToCart) {
+      addToCart(product);
+    } else {
+      console.error("addToCart function is not defined.");
+    }
+  };
+  
   return (
     <div>
       <section className="featured-products">
         <h2>Featured Products</h2>
         <div className="products-grid">
-          {displayedProducts && displayedProducts.map((product) => (
-            <Link to={`/product/${product.id}`} key={product.id}>
-              <div className="product-card" >
-                <img src={product.image_url} alt="Product" />
+          {displayedProducts &&
+            displayedProducts.map((product) => (
+              <div className="product-card" key={product.id}>
+                <Link to={`/product/${product.id}`}>
+                  <img src={product.image_url} alt="Product" />
+                </Link>
                 <h3>{product.name}</h3>
                 <p>R{product.price}</p>
-                <button className="add-to-cart-button">Add to Cart</button>
+                <button className="add-to-cart-button" onClick={()=>handleAddToCartBtn(product)}>Add to Cart</button>
               </div>
-            </Link>
-          ))}
+            ))}
         </div>
       </section>
     </div>
